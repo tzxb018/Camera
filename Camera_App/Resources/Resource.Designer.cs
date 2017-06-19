03cc0c06b535d53d65f32d10c142f2a539c67a47
Tomo Bessho <tzxbApp018@gmail.com>
2017-06-19 11:16:14 -0500

    UI Update for New Feature

--- Camera_App/Resources/Resource.Designer.cs
+++ Camera_App/Resources/Resource.Designer.cs
@@ -60,11 +60,17 @@ namespace Camera_App
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int btnSnap = 2131034113;
+			// aapt resource value: 0x7f050003
+			public const int btnGallery = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int btnSnap = 2131034114;
 			
 			// aapt resource value: 0x7f050000
 			public const int imgView = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int linearLayout2 = 2131034113;
 			
 			static Id()
 			{
